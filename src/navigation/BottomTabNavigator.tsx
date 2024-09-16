aca0aaefa895213095d4f29e5f4059c5a46ba869
hgkim <hgkim1598@gamil.com>
2024-09-16 19:38:12 +0900

    FEAT 네비게이션 메뉴에 아이콘 추가 테스트 (홈)

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -5,13 +5,21 @@ import CalendarScreen from '../screens/Calendar/CalendarScreen';
 import DiaryScreen from '../screens/Diary/DiaryScreen';
 import ChatbotScreen from '../screens/Chatbot/ChatbotScreen';
 import MyPageScreen from '../screens/Mypage/MypageScreen';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
   return(
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" color={color} size={size} />
+          ),
+        }} />
       <Tab.Screen name="Calendar" component={CalendarScreen} />
       <Tab.Screen name="Diary" component={DiaryScreen} />
       <Tab.Screen name="Chatbot" component={ChatbotScreen} />
